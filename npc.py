1c472e8fd7df7197a8ae321b12c50df26536101f
Luka Kukec <lkukec22@student.foi.hr>
2025-04-09 10:26:40 +0200

    Dodavanje klonova u igru, treba popravak slika

--- npc.py
+++ npc.py
@@ -193,6 +193,11 @@ class SoldierNPC(NPC):
                  scale=0.6, shift=0.38, animation_time=180):
         super().__init__(game, path, pos, scale, shift, animation_time)
 
+class KlonoviNPC(NPC):
+    def __init__(self, game, path='resources/sprites/npc/klonovi/0.png', pos=(10.5, 5.5),
+                 scale=0.6, shift=0.38, animation_time=180):
+        super().__init__(game, path, pos, scale, shift, animation_time)
+
 class CacoDemonNPC(NPC):
     def __init__(self, game, path='resources/sprites/npc/caco_demon/0.png', pos=(10.5, 6.5),
                  scale=0.7, shift=0.27, animation_time=250):
