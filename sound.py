b555b686c792b8d60de7042f43906a26e5033929
Luka Hrupec <luka.hrupec1@gmail.com>
2025-04-09 11:05:09 +0200

    Postavljen novi zvuk za neprijatelja

--- sound.py
+++ sound.py
@@ -8,9 +8,10 @@ class Sound:
         self.path = 'resources/sound/'
         self.pistolj = pg.mixer.Sound(self.path + 'Pistolj.wav')
         self.npc_pain = pg.mixer.Sound(self.path + 'npc_pain.wav')
-        self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
+        self.npc_death = pg.mixer.Sound(self.path + 'stakor_smrt.mp3')
+        self.npc_death.set_volume(0.9)  
         self.npc_shot = pg.mixer.Sound(self.path + 'npc_attack.wav')
-        self.npc_shot.set_volume(0.2)
+        self.npc_shot.set_volume(0.1) 
         self.igrac_damage = pg.mixer.Sound(self.path + 'Igrac_damage.wav')
         self.razina1 = pg.mixer.music.load(self.path + 'Pozadinska1.mp3')
         self.player_dash = pg.mixer.Sound(self.path + 'Dash.wav')  
